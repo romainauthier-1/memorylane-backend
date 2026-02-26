eb1f30ed10a137fa309625d6d14c573f636def90
RomainAuthier <romainauthier@outlook.com>
2026-02-26 22:43:30 +0100

    Modif route upload pour pointer vers fichier temporaire système

--- routes/upload.js
+++ routes/upload.js
@@ -4,17 +4,13 @@ const path = require("path");
 const uniqid = require("uniqid");
 const cloudinary = require("cloudinary").v2;
 const fileSystem = require("fs");
+const os = require("os");
 
 // POST créer url du fichier envoyé
 router.post("/", async (req, res) => {
   // console.log("POST /upload, fichier envoyé: ", req.files?.file);
   const extension = path.extname(req.files?.file.name);
-  const photoPath = path.join(
-    __dirname,
-    "..",
-    "tmp",
-    `${uniqid()}${extension}`,
-  );
+  const photoPath = path.join(os.tmpdir(), `${uniqid()}${extension}`);
   // console.log("Chemin utilisé: ", photoPath);
   const resultMove = await req.files.file.mv(photoPath);
 
