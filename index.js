bd7988de46e879a533cac1325b22e2eded67da8e
RomainAuthier <romainauthier@outlook.com>
2026-04-11 22:20:59 +0200

    Augmentation upload

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.get("/", (req, res) => {
   res.send("Le serveur des souvenirs est en ligne ! 🚀");
 });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // app.use(fileUpload());
 app.use(
